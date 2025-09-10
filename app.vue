d91264c27839b20a6cbced080f7d9dae379564b0
Kumar Gaurav <kmrgaurav33@gmail.com>
2025-09-10 18:06:25 +0530

    feat: chagned heading

--- app.vue
+++ app.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <h1>
-      NeoPaper!
-    </h1>
-    <p>Coming soon...</p>
+  <div class="app">
+    <h1>NeoPaper!</h1>
+    <p>Under construction...</p>
+    <h4>Build by <a href="http://x.com/QurioSapien">QurioSapien</a> </h4>
   </div>
 </template>
+
+<style scoped>
+
+.app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,14 @@ export default defineNuxtConfig({
     // prerender index route by default
     '/': { prerender: true },
   },
+  app: {
+    head: {
+      title: 'NeoPaper',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: 'utf-8' },
+        { name: 'description', content: 'A Nuxt 3 project with Async Data fetching and Tailwind CSS' },
+      ]
+    },
+  }
 });
